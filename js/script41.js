bdfc80af3a3ff3e687dbb1c1b11b7bdcb92e679a
franz <francesco.scarfato@elitedivision.it>
2025-07-01 17:48:19 +0200

    Aggiunti files ex 4.1

---
+++ js/script41.js
@@ -0,0 +1,41 @@
+let val1 = 11;
+let val2 = 1;
+let val3 = 2;
+let val4 = 3;
+let val5 = 4;
+
+let totale = val1 + val2 + val3 + val4 + val5;
+let media = totale / 5;
+let moltiplico15 = val1 * val5;
+let divido34 = val3 / val4;
+
+console.log('La somma totale di ' + val1
+    + ' + ' + val2
+    + ' + ' + val3
+    + ' + ' + val3
+    + ' + ' + val5
+    + ' è = ' + totale
+    + ' la media è: ' + media
+);
+
+console.log('La moltiplicazione tra ' + val1
+    + ' * ' + val5 
+    + ' è = ' + moltiplico15
+);
+
+console.log('La divisione tra ' + val3
+    + ' / ' + val4
+    + ' è = ' + divido34
+);
+
+console.log(val1 + ' > ' + val5 
+    + ' è ' + (val1 > val5)
+);
+
+console.log(val1 + ' < ' + val2
+    + ' è ' + (val1 < val2)
+);
+
+console.log(val1 + ' = ' + val5
+    + ' è ' + (val1 == val5)
+);
